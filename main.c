45c80579cc9b95a995cbcf6200b3cd2e1a9970a3
UraYukimitsu <>
2014-04-16 15:01:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include "gamemap.h"
 #include "titleScreen.h"
 #include "options.h"
+#include "gameOverScreen.h"
 
 #define array(i, j) (*(arrayAccess + (4 * i) + (j)))
 #define array1(i, j) (*(array1Access + (4 * i) + (j)))
@@ -23,8 +24,10 @@ void backGrid();
 void invertGrid();
 void rotateGrid();
 void updateScore(UINT16 sc, UINT8 pos);
-UINT8 randCell();
+void dispGameOverScreen(UINT8* text);
 UINT16 pow(UINT16 x, UINT16 y);
+UINT8 gameOver();
+UINT8 randCell();
 UINT8 compare();
 
 UINT8 numberTile[4];
@@ -47,13 +50,15 @@ void main()
 {
 	UINT8 joyState;
 	bestScore = 0;
-	start:
-	score = 0;
-	undo = 2;
-	maxTile = 11;
-	
+	maxTile = 4;
 	set_bkg_data(0, 125, tiles);
 	dispTitleScreen();
+	
+	start:
+	
+	score = 0;
+	undo = 2;
+
 	set_bkg_tiles(0, 0, 20, 18, gamemap);
 	
 	for(i = 0; i<4; i++)
@@ -118,8 +123,15 @@ void main()
 						score += pow(2, array(y, i) - 1);
 						if(score >= bestScore)
 							bestScore = score;
-						if(array[y][i] > maxTile)
+						if(array[y][i] > maxTile - 1)
+						{
+							updateScreen();
+							waitpadup();
+							waitpad(J_A|J_B|J_SELECT|J_START|J_UP|J_DOWN|J_LEFT|J_RIGHT);
+							waitpadup();
+							dispGameOverScreen(".......you.win......");
 							goto start;
+						}
 						for(j = 0; j < i; j++)
 						{
 							if(!array(y, i - j))
@@ -132,8 +144,16 @@ void main()
 				}
 			}
 			if(compare())
-				if(randCell())
-					goto start;
+				randCell();
+			if(gameOver())
+			{
+				updateScreen();
+				waitpadup();
+				waitpad(J_A|J_B|J_SELECT|J_START|J_UP|J_DOWN|J_LEFT|J_RIGHT);
+				waitpadup();
+				dispGameOverScreen("......game.over.....");
+				goto start;
+			}
 		}
 		
 		if(joyState&J_UP || joyState&J_DOWN)
@@ -193,6 +213,50 @@ UINT8 compare()
 			if(array1(i, j) != array(i, j))
 				return 1;
 	return 0;
+}
+
+UINT8 gameOver()
+{
+	UINT8 count = 1;
+	for(i = 0; i < 4; i++)
+		for(j = 0; j < 4; j++)
+			if(!array(i, j))
+				return 0;
+	for(i = 0; i < 4; i++)
+		for(j = 0; j < 4; j++)
+		{
+			switch(i)
+			{
+				case 0:
+					if(array(i, j) == array(i + 1, j))
+						count = 0;
+					break;
+				case 3:
+					if(array(i, j) == array(i - 1, j))
+						count = 0;
+					break;
+				default:
+					if(array(i, j) == array(i - 1, j) || array(i, j) == array(i + 1, j))
+						count = 0;
+					break;
+			}
+			switch(j)
+			{
+				case 0:
+					if(array(i, j) == array(i, j + 1))
+						count = 0;
+					break;
+				case 3:
+					if(array(i, j) == array(i, j - 1))
+						count = 0;
+					break;
+				default:
+					if(array(i, j) == array(i, j - 1) || array(i, j) == array(i, j + 1))
+						count = 0;
+					break;
+			}
+		}
+	return count;
 }
 
 UINT16 pow(UINT16 x, UINT16 y)
@@ -335,5 +399,100 @@ void dispTitleScreen()
 		}
 		maxTile = choice + 10;
 	}
-	initarand(clock());
+	initarand(clock() >> 8);
+}
+
+void dispGameOverScreen(UINT8 *text)
+{
+	UINT8 choice, cont, joyState;
+	tile[0] = 123;
+	choice = 0;
+	cont = 1;
+	set_bkg_tiles(0, 0, 20, 18, gameOverScreen);
+	updateScore(score, 6);
+	updateScore(bestScore, 18);
+	for(i = 0; i < strlen(text); i++)
+	{
+		if(i < 20)
+		{
+			if(text[i] >= 'a' && text[i] <= 'z')
+				tile[0] = text[i] - 'a' + 97;
+			else
+				tile[0] = 0;
+			set_bkg_tiles(i, 6, 1, 1, tile);
+		}
+	}
+	tile[0] = 123;
+	SHOW_BKG;
+	while(cont)
+	{
+		joyState = joypad();
+		if(joyState&J_A)
+			cont = 0;
+		if(joyState&J_DOWN || joyState&J_UP)
+		{
+			if(!choice)
+			{
+				choice = 1;
+				set_bkg_tiles(6, 12, 1, 1, tile);
+				tile[0] = 0;
+				set_bkg_tiles(6, 10, 1, 1, tile);
+				tile[0] = 123;
+			}
+			else
+			{
+				choice = 0;
+				set_bkg_tiles(6, 10, 1, 1, tile);
+				tile[0] = 0;
+				set_bkg_tiles(6, 12, 1, 1, tile);
+				tile[0] = 123;
+			}
+		}
+		while(joyState)
+		{
+			joyState = joypad();
+		}
+	}
+	joyState = 1;
+	while(joyState)
+	{
+		joyState = joypad();
+	}
+	cont = 1;
+	
+	if(choice)
+	{
+		set_bkg_tiles(0, 0, 20, 18, optionScreen);
+		choice = 0;
+		while(cont)
+		{
+			joyState = joypad();
+			if(joyState&J_B)
+				cont = 0;
+			if(joyState&J_DOWN)
+			{
+					set_bkg_tiles(10, 2*((choice + 1)%7) + 2, 1, 1, tile);
+					tile[0] = 0;
+					set_bkg_tiles(10, 2*choice + 2, 1, 1, tile);
+					tile[0] = 123;
+					choice = ++choice % 7;
+			}
+			if(joyState&J_UP)
+			{
+				tile[0] = 0;
+				set_bkg_tiles(10, 2*choice + 2, 1, 1, tile);
+				if(choice == 0)
+					choice = 7;
+				tile[0] = 123;
+				set_bkg_tiles(10, 2*(choice - 1) + 2, 1, 1, tile);
+				choice = --choice;
+			}
+			while(joyState)
+			{
+				joyState = joypad();
+			}
+		}
+		maxTile = choice + 10;
+	}
+	initarand(clock() >> 8);
 }
